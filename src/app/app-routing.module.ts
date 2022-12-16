e57760ef2c488b0041506b7b0547d1ee81922f0f
Kingsley Darko <nyaabilai@gmail.com>
2022-12-16 16:22:12 +0000

    added about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperienceComponent } from './experience/experience.component';
 import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/experience', pathMatch: 'full' },
   { path:'experience',component:ExperienceComponent },
   { path:'education',component:EducationComponent },
-  { path:'skills',component:SkillsComponent },
+  { path:'about',component:AboutComponent },
   { path:'projects',component:ProjectsComponent },
 ];
 
@@ -22,6 +22,6 @@ export class AppRoutingModule { }
 export const routingComponents=[
   ExperienceComponent,
   EducationComponent,
-  SkillsComponent,
+  AboutComponent,
   ProjectsComponent
 ]
